3b23a33717f141ec96f18c3bf3fb78e3692b4c13
Frankie239 <55329447+Frankie239@users.noreply.github.com>
2020-06-19 13:33:16 -0300

    Creado un dummy de db para ir probando cosas, linkeado el boton de crear usuario con el controlador de usuarios y el metodo crear.

---
+++ miniForo/Models/dbDummyUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace miniForo.Models
+{
+    public class dbDummyUser
+    {
+        public string name { set; get; }
+
+        public string email { set; get; }
+
+        public string creationDate { set; get; }
+        public int karma { set; get; }
+
+        public string password { set; get; }
+      
+    }
+}
