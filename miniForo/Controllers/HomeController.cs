7034b9610278a96769352b2f73b4bcc8f216151b
Frankie239 <55329447+Frankie239@users.noreply.github.com>
2020-06-20 00:20:30 -0300

    Agregado un dbDummy de las entradas del foro, una vista cargar entrada, la vista crear usuario quedo mejor y la pagina de log in ya envia los datos al controlador de Log In para luego aplicar la logica

--- miniForo/Controllers/HomeController.cs
+++ miniForo/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using miniForo.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,8 +12,25 @@ namespace miniForo.Controllers
         // GET: Home
         public ActionResult landing()
         {
-            ViewBag.style = "landing.css";
+            ViewBag.title = "LOREM IMPSUM";
+            ViewBag.description = "a little description over here";
+            ViewBag.text = "Probando probando probando probando probando.";
+            ViewBag.userTag = "@FranGimen";
+
+         
             return View();
+        }
+
+        public ActionResult LoadEntry()
+        {
+            ViewBag.title = "LOREM IMPSUM";
+            ViewBag.description ="a little description over here";
+            ViewBag.text = "Probando probando probando probando probando.";
+            ViewBag.userTag ="@FranGimen";
+
+            return View();
+
+
         }
     }
 }
--- miniForo/Controllers/UsersController.cs
+++ miniForo/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using miniForo.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,25 @@ namespace miniForo.Controllers
     public class UsersController : Controller
     {
         // GET: Users
+
         public ActionResult createUser()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult createUser(dbDummyUser user)
         {
             return View();
         }
+
+        public ActionResult LogIn()
+        {
+
+            //Esto es si todo sale bien, recarga la pagina de home
+            return RedirectToAction("Landing", "Home");
+        }
     }
+
 }
