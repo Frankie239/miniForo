56daa5777732797018c91a8811faa6960ce67b39
Frankie239 <55329447+Frankie239@users.noreply.github.com>
2020-06-21 15:33:08 -0300

    Commit con un poco de la logica del inicio de sesion, pero esta roto, no funciona como deberia.

--- miniForo/Controllers/UsersController.cs
+++ miniForo/Controllers/UsersController.cs
@@ -1,9 +1,14 @@
 ﻿using miniForo.Models;
+using miniForo.Models.DAL;
+using miniForo.Models.Exceptions;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using System.Web.Services.Description;
 
 namespace miniForo.Controllers
 {
@@ -23,9 +28,48 @@ namespace miniForo.Controllers
             return View();
         }
 
-        public ActionResult LogIn()
+        [HttpPost]
+        public ActionResult LogIn(string email, string password)
         {
+            User user = new User();
+            Password pass = new Password();
+            string message = "";
+            ViewBag.message = message;
+            
 
+
+
+            using (BlogContext db = new BlogContext())
+            {
+
+                user = db.User.FirstOrDefault(e => e.email == email);
+                //pass = db.Password.SqlQuery("SELECT password FROM [Password] WHERE [userId] = @tag", user.userTag);
+
+
+
+
+
+            }
+         
+
+
+            if(user.email == email)
+            {
+                if(passwords == password)
+                {
+                    FormsAuthentication.SetAuthCookie(user.email, true);
+                    return RedirectToAction("UserLogedIn", "Profile");
+                }
+              
+            }
+           
+            else
+            {
+                message = "User or credentials not found or credentials incorrect";
+                //throw new UserNotFoundException();
+                
+
+            }
             //Esto es si todo sale bien, recarga la pagina de home
             return RedirectToAction("Landing", "Home");
         }
